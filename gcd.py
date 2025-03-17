686057c5da507239516ad558bb6828231010418f
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2025-03-17 21:40:34 +0100

    Improve gcd.py
    - Give indications about why the properties hold.
    - In binary_gcd, the second loop was stating a_i b_i both odd, which
      wasn't guaranteed to be true on the first iteration. Change the
      previous loop to make it so.
    - Make testing more flexible wrt the number of functions to test.
    (This is in preparation for egcd.)

--- gcd.py
+++ gcd.py
@@ -6,10 +6,16 @@ import secrets
 # 2. gcd(a, a) = a
 # 3. gcd(a, b) = gcd(b, a)
 # 4. gcd(a, b) = gcd(a, b - a)
+#       if d divides a and b, it also divides b - a
+#       if d divides a and b - a, it also divdes a + (b-a) = b
 # 5. gcd(a, b) = gcd(a, b - n*a) for any integer n
+#       iterated version of the above
 # 6. gcd(a, b) = gcd(a, b % a)
+#       b % a is b - n*a for some n
 # 7. gcd(a, b) = 2 * gcd(a / 2, b / 2) if a and b both even
+#       factor 2 in common
 # 8. gcd(a, b) = gcd(a, b / 2) if a odd and b even
+#       the factors 2 in b can't contribute to the GCD since a is odd
 
 
 # https://en.wikipedia.org/wiki/Euclidean_algorithm
@@ -37,42 +43,48 @@ def binary_gcd(a, b):
     if b == 0:
         return a
 
-    # Invariant: g_i * gcd(a_i, b_i) = gcd(a, b)
-    # Variant: bitlen(a) and bitlen(b) decreasing
-    g = 1
-    while a & 1 == 0 and b & 1 == 0:
+    # Take the factors 2 in a and b
+    i = 0
+    while a & 1 == 0:
         a >>= 1
+        i += 1
+    j = 0
+    while b & 1 == 0:
         b >>= 1
-        g <<= 1
+        j += 1
+    g = 1 << min(i, j)
 
-    # Invariant: a_i >= b_i both odd, and gcd(a_i, b_i) = gcd(a_{i-1}, b_{i-1})
+    # Invariant: a_i and b_i both odd, and gcd(a_i, b_i) = gcd(a_{i-1}, b_{i-1})
     # Variant: bitlen(a) decreasing
     while a != b:
+        if b > a:
+            a, b = b, a
+
         a -= b
         while a & 1 == 0:
             a >>= 1
 
-        if b > a:
-            a, b = b, a
-
     return g * a
 
 
-def test(a, b):
-    ref = math.gcd(a, b)
-    euc = euclid_gcd(a, b)
-    bnr = binary_gcd(a, b)
-    assert ref == euc, f"{a} {b} -> {ref} != {euc}"
-    assert ref == bnr, f"{a} {b} -> {ref} != {bnr}"
+def test_one(func, name, a, b):
+    exp = math.gcd(a, b)
+    got = func(a, b)
+    assert got == exp, f"{name}({a}, {b}) = {got} != {exp}"
 
 
-for a in range(10):
-    for b in range(10):
-        test(a, b)
+def test(func, name):
+    for a in range(10):
+        for b in range(10):
+            test_one(func, name, a, b)
 
-for _ in range(100):
-    a = secrets.randbits(256)
-    b = secrets.randbits(256)
-    test(a, b)
+    for _ in range(100):
+        a = secrets.randbits(256)
+        b = secrets.randbits(256)
+        test_one(func, name, a, b)
+
+
+test(euclid_gcd, "euclid_gcd")
+test(binary_gcd, "binary_gcd")
 
 print(math.gcd(66528, 52920))
