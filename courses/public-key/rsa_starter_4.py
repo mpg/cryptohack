8e005fa4a7f15f059f28feb543858c9a591ea964
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2025-10-24 00:07:42 +0200

    rsa_starter_4

---
+++ courses/public-key/rsa_starter_4.py
@@ -0,0 +1,17 @@
+import math
+from utils import modinv
+
+p = 857504083339712752489993810777
+q = 1029224947942998075080348647219
+e = 65537
+
+n = p * q
+
+phi_n = (p - 1) * (q - 1)
+d = modinv(e, phi_n)
+print(d)
+
+lambda_n = math.lcm(p - 1, q - 1)
+d_min = modinv(e, lambda_n)
+print(d_min)
+
---
+++ courses/public-key/utils.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                             os.path.pardir, 'modular'))
+
+from modinv import modinv_euclid as modinv
